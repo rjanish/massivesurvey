1177e88b8c35fd24165c3a692e235ce5b9339bf5
Ryan Janish <ryan.janish@gmail.com>
2015-03-12 12:00:56 -0700

    edit and expand seriesfit init doc string

--- massive/seriesfit.py
+++ massive/seriesfit.py
@@ -8,7 +8,7 @@ class SeriesFit(object):
     sum of multiple localized and identically-shaped sub-models.
 
     The model $F$ should take the form:
-      $ F(x; P) = b(x; p_b) + \sum_{i=1}^N f(x; p_i) $.
+      $ F(x; P) = b(x; p_b) + \sum_{i=0}^{N-1} f(x; p_i) $.
     Here x is the data. b is a slowly-varying background model with a
     set of $n_b$ parameters denoted collectively by $p_b$. Similarly,
     f is a localized model (such as a Gaussian peak) dependent on a
@@ -28,7 +28,7 @@ class SeriesFit(object):
     in one sub-region and fit to a sum of multiple sub-models.
     """
     def __init__(self, x, y, submodel, width, center,
-                 initial_guess, width_criteria, minimizer):
+                 initial_guess, minimizer, width_criteria=5.0):
         """
         Args:
         x - arraylike
@@ -36,24 +36,23 @@ class SeriesFit(object):
         y - arraylike
             Observed independent variable
         submodel - func
-            The localized model, of which a sum of multiple instances
-            comprises the full model.  The call syntax must be
-            submodel(arg, params), where arg is the dependent variable
-            and params is an array of parameters.
+            The localized sub-model. The calling syntax must be
+            submodel(arg, params), where arg is a dependent variable
+            array and params is an array of parameters.
         center - func
             A function to determine the central value, in units of the
             dependent variable, of a single instance of a submodel.
-            The call syntax must be center(params), with params being
-            the parameters of the submodel in question.
+            The calling syntax must be center(params), with params
+            being the parameters of the submodel in question.
         width - func
             Similar to 'center' above, but returning the width in
             units of the dependent variable of the submodel.
         initial_guess - arraylike
             An array of initial guesses for the parameters of each
-            submodel, i.e. initial_guess[n] is the set of initial
-            parameter guesses for submodel n.  The length of the first
+            submodel, i.e. initial_guess[n] is an array of initial
+            parameter guesses for submodel n. The length of the first
             axis determines the number of submodels to be used.
-        width_criteria - float
+        width_criteria - float, default=5.0
             This determines the size of the fitting region used for
             each submodel. Each region has size width_criteria*width,
             where width is the width of the submodel in that region.

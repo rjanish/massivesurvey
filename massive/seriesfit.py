7d698d01736e99be655f4ba66ddd9509525f0277
Ryan Janish <ryan.janish@gmail.com>
2015-03-12 11:56:59 -0700

    edit and expand seriesfit doc string

--- massive/seriesfit.py
+++ massive/seriesfit.py
@@ -1,10 +1,31 @@
-
+"""
+Module for fitting data a model which is a sum of identical sub-models.
+"""
 
 class SeriesFit(object):
     """
-    Class for computing fits to models that are sums of some localized
-    sub-model, such as a series of Gaussian peaks centered at various
-    different locations.
+    This class simplifies fitting data to models which consist of a
+    sum of multiple localized and identically-shaped sub-models.
+
+    The model $F$ should take the form:
+      $ F(x; P) = b(x; p_b) + \sum_{i=1}^N f(x; p_i) $.
+    Here x is the data. b is a slowly-varying background model with a
+    set of $n_b$ parameters denoted collectively by $p_b$. Similarly,
+    f is a localized model (such as a Gaussian peak) dependent on a
+    set of $n_p$ parameters. There are $N$ such sub-models, each with
+    an independent parameter set denoted $p_i$ for the ith sub-model.
+    This parameters set $p_i$ must determine $f$'s size and central
+    location. The data will be fit to determine the $N n_p + n_b$
+    parameters of the full model.
+
+    The sub-model $f$ is assumed to describe the interesting data
+    features, with $b$ an uninteresting background. Fitting to $b$
+    is avoided as much as possible to improve performance. This is
+    done by partitioning the data into sub-regions and fitting one $f$
+    per region independent of all other regions. The region sizes are
+    set iteratively using the widths of the best-fitting sub-models.
+    If two or more features are overlapping, they will be included
+    in one sub-region and fit to a sum of multiple sub-models.
     """
     def __init__(self, x, y, submodel, width, center,
                  initial_guess, width_criteria, minimizer):

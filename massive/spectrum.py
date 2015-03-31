ad096e16fcc7906eb4cb2a7e6d5f86dd7ab4c85e
Ryan Janish <ryan.janish@gmail.com>
2015-03-31 16:30:55 -0700

    added spectrum/waves sanitizing

--- massive/spectrum.py
+++ massive/spectrum.py
@@ -14,3 +14,33 @@ class SpectrumSet(object):
     methods for I/O and manipulation of spectra are meant to enforce
     the preservation and automatic updating of the metadata as needed.
     """
+    def __init__(self):
+        """
+        Mandatory arguments here force explicit recording of metadata.
+        When this function returns, the object will hold all of the
+        spectra metadata relevant for kinematic modeling.
+
+        Args:
+        data - 2d arraylike
+            The actual spectrum values. Each spectrum occupies a row,
+            and are assumed to be identical in sampling and units. A
+            single 1d array may be passed, which is interpreted as a
+            set containing a single spectrum and is converted as such.
+        wavelengths - 1d arraylike
+            Wavelength sample values, the same for each spectrum.
+        """
+        # check spectra
+        self.spectra = np.asarray(spectra)
+        if self.spectra.ndim == 1:
+            self.spectra = np.expand_dims(spectra, 0)
+        elif self.spectra.ndim != 2:
+            raise ValueError("Invalid data array shape: {}, must have either"
+                             " 1 or 2 dimensions".format(self.spectra.shape))
+        self.num_spectra, self.num_samples = self.spectra.shape
+        # check waves
+        self.waves = np.asarray(wavelengths)
+        if ((self.waves.ndim != 1) or
+            (self.waves.size != self.num_samples)):
+            raise ValueError("Invalid wavelength array shape: {}\n"
+                             "Must be 1D and match the size of spectra: {}"
+                             "".format(self.waves.shape, self.num_samples))

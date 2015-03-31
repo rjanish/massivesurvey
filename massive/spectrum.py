674df141cd136e864a0c15689cfcb29fdd176faa
Ryan Janish <ryan.janish@gmail.com>
2015-03-31 16:18:47 -0700

    added spectrumset docs

---
+++ massive/spectrum.py
@@ -0,0 +1,16 @@
+"""
+This module handles the storage and common manipulations of spectra
+and collections of spectra.
+"""
+
+
+class SpectrumSet(object):
+    """
+    This class holds a set of spectra along with their metadata.
+
+    The metadata are the focus here - this class mainly serves to
+    enforce that all of the information needed to interpret spectra is
+    explicitly recorded along with the spectral data. The included
+    methods for I/O and manipulation of spectra are meant to enforce
+    the preservation and automatic updating of the metadata as needed.
+    """

8a406b9ee8648a659cfc4d9433a12ab397251564
Ryan Janish <ryan.janish@gmail.com>
2015-04-01 09:56:39 -0700

    spectrum now sucessfully initialized data, waves, comments

--- massive/spectrum.py
+++ massive/spectrum.py
@@ -4,6 +4,9 @@ and collections of spectra.
 """
 
 
+import numpy as np
+
+
 class SpectrumSet(object):
     """
     This class holds a set of spectra along with their metadata.
@@ -14,14 +17,14 @@ class SpectrumSet(object):
     methods for I/O and manipulation of spectra are meant to enforce
     the preservation and automatic updating of the metadata as needed.
     """
-    def __init__(self, data, wavelengths, comments={}):
+    def __init__(self, spectra, wavelengths, comments={}):
         """
         Mandatory arguments here force explicit recording of metadata.
         When this function returns, the object will hold all of the
         spectra metadata relevant for kinematic modeling.
 
         Args:
-        data - 2d arraylike
+        spectra - 2d arraylike
             The actual spectrum values. Each spectrum occupies a row,
             and are assumed to be identical in sampling and units. A
             single 1d array may be passed, which is interpreted as a
@@ -39,15 +42,15 @@ class SpectrumSet(object):
         if self.spectra.ndim == 1:
             self.spectra = np.expand_dims(spectra, 0)
         elif self.spectra.ndim != 2:
-            raise ValueError("Invalid data array shape: {}, must have either"
-                             " 1 or 2 dimensions".format(self.spectra.shape))
+            raise ValueError("Invalid data array shape: {}. Must be either "
+                             "1 or 2 dimensions.".format(self.spectra.shape))
         self.num_spectra, self.num_samples = self.spectra.shape
         # check waves
         self.waves = np.asarray(wavelengths)
         if ((self.waves.ndim != 1) or
             (self.waves.size != self.num_samples)):
-            raise ValueError("Invalid wavelength array shape: {}\n"
-                             "Must be 1D and match the size of spectra: {}"
+            raise ValueError("Invalid wavelength array shape: {}. "
+                             "Must be 1D and match the size of spectra: {}."
                              "".format(self.waves.shape, self.num_samples))
         # remaining arg checks
         self.comments = {str(k):v for k, v in comments.iteritems()}

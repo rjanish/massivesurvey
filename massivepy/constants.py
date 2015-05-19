0eeb82cae871af9b08093c765488ea3f9402de2f
Ryan Janish <ryan.janish@gmail.com>
2015-05-18 21:57:46 -0700

    added mitchell fiber radius const

--- massivepy/constants.py
+++ massivepy/constants.py
@@ -5,7 +5,11 @@ Miscellaneous constants and unit definitions of general use.
 
 import astropy.units as units
 
+# units
 arcsec = units.arcsec
 cgs_flux = units.erg/(units.second*(units.cm**2))
 angstrom = units.angstrom
 flux_per_angstrom = cgs_flux/units.angstrom
+
+# instrument constants
+mitchell_fiber_radius = 2.08 * arcsec

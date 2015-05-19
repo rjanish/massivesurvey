1cbad5785d6666f04e762fc9c80e5fb5556d926a
Ryan Janish <ryan.janish@gmail.com>
2015-05-18 21:34:35 -0700

    added arcsec

--- massivepy/constants.py
+++ massivepy/constants.py
@@ -5,7 +5,7 @@ Miscellaneous constants and unit definitions of general use.
 
 import astropy.units as units
 
-
+arcsec = units.arcsec
 cgs_flux = units.erg/(units.second*(units.cm**2))
 angstrom = units.angstrom
 flux_per_angstrom = cgs_flux/units.angstrom

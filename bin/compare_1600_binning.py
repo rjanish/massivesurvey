6f3dff1121ebd143f7bb1b1dc0d43c64d644ccde
Ryan Janish <ryan.janish@gmail.com>
2015-05-20 11:42:52 -0700

    added script to compare fall2014-vs-now 1600 binning difference

---
+++ bin/compare_1600_binning.py
@@ -0,0 +1,15 @@
+"""
+This script compare the bin layout and resulting kinematics for two
+binnings of NGC 1600.
+
+The first of these binning was made by Stephen (ADD DETAILS) and fit
+by Ryan Janish in May 2015. This used binning code dating from August
+2014 (CHECK).  The second was binning by Ryan Janish on May 20 2015,
+using binning code dating from May 20 2015.
+"""
+
+
+import utilities as utl
+
+
+# binned 2014-08

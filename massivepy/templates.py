873aa84905dd0784962affd32769e2474a2c9094
Ryan Janish <ryan.janish@gmail.com>
2015-05-27 08:53:28 -0700

    rearrange

--- massivepy/templates.py
+++ massivepy/templates.py
@@ -15,6 +15,78 @@ import massivepy.spectrum as spec
 import massivepy.constants as const
 
 
+class TemplateLibrary(object):
+    """
+    This class holds a stellar template library, and provides methods
+    for manipulations commonly needed on such libraries.
+    """
+    def __init__(self, catalog=None, **kwargs):
+        """
+        See SpectrumSet. Arguments needed beyond those of SpectrumSet
+        are described below.
+
+        Args:
+        spectrumset - SpectrumSet object or keyword args
+            The spectral data, either as a SpectrumSet object, or by
+            passing all of the keyword arguments needed by SpectrumSet
+        catalog - pandas dataframe
+            A dataframe giving the properties of the spectra in the
+            template library. The columns of the dataframe are assumed
+            to be the properties, and the index of the dataframe must
+            match in order the ids of the accompanying SpectrumSet.
+        """
+        if 'spectrumset' in kwargs:
+            self.spectrumset = kwargs['spectrumset']
+        else:
+            self.spectrumset = spec.SpectrumSet(**kwargs)
+        self.catalog = pd.DataFrame(catalog)
+        index_array = self.catalog.index.values
+        index_matches = np.all(index_array == self.spectrumset.ids)
+        if not index_matches:
+            raise ValueError("Invalid catalog index - does "
+                             "not match the given spectral ids")
+
+    def get_subset(self, ids):
+        """
+        Extract subset of the library with the passed spectrum ids. A
+        new TemplateLibrary will be returned.
+
+        For details, see SpectrumSet.get_subset.
+        """
+        new_set, index = self.spectrumset.get_subset(ids, get_selector=True)
+        new_catalog = self.catalog[index]
+        return TemplateLibrary(spectrumset=new_set, catalog=new_catalog)
+
+    def match_resolution(self, target_resolution):
+        """
+        Convolve the library spectra until their spectra resolution
+        matches that passed target_resolution. Does not return -
+        modifies the library spectra in place.
+
+        Args:
+        target_resolution - 1D arraylike
+            The desired spectral resolution, sampled with wavelength
+            identically to the library spectra. The units are assumed
+            to be Gaussian FWHM in the wavelength units of the library.
+        """
+        target_fwhm = np.asarray(target_resolution)
+        current_resolution = self.spectrumset.metaspectra["ir"]
+        delta_var = target_resolution**2 - current_resolution**2
+        res_matches = np.max(np.absolute(delta_var)) < const.float_tol
+        if res_matches:
+            warnings.warn("Templates already have the target spectral "
+                          "resolution, skipping convolution", RuntimeWarning)
+            return
+        decrease_fwhm = np.any(delta_var < 0.0)
+        if decrease_fwhm:
+            raise ValueError("Invalid target resolution - must be "
+                             "greater than the current resolution")
+        fwhm_to_add = np.sqrt(delta_var)
+        sigma_to_add = fwhm_to_add/const.gaussian_fwhm_over_sigma
+        self.spectrumset = self.spectrumset.gaussian_convolve(sigma_to_add)
+        return
+
+
 def miles_number_to_filename(num):
     """
     Convert an id number from the MILES stellar template library into
@@ -99,75 +171,3 @@ def read_miles_library(dirname):
                            spectra_unit=const.flux_per_angstrom,
                            wavelength_unit=const.angstrom, comments=comments,
                            name=library_name, catalog=catalog)
-
-
-class TemplateLibrary(object):
-    """
-    This class holds a stellar template library, and provides methods
-    for manipulations commonly needed on such libraries.
-    """
-    def __init__(self, catalog=None, **kwargs):
-        """
-        See SpectrumSet. Arguments needed beyond those of SpectrumSet
-        are described below.
-
-        Args:
-        spectrumset - SpectrumSet object or keyword args
-            The spectral data, either as a SpectrumSet object, or by
-            passing all of the keyword arguments needed by SpectrumSet
-        catalog - pandas dataframe
-            A dataframe giving the properties of the spectra in the
-            template library. The columns of the dataframe are assumed
-            to be the properties, and the index of the dataframe must
-            match in order the ids of the accompanying SpectrumSet.
-        """
-        if 'spectrumset' in kwargs:
-            self.spectrumset = kwargs['spectrumset']
-        else:
-            self.spectrumset = spec.SpectrumSet(**kwargs)
-        self.catalog = pd.DataFrame(catalog)
-        index_array = self.catalog.index.values
-        index_matches = np.all(index_array == self.spectrumset.ids)
-        if not index_matches:
-            raise ValueError("Invalid catalog index - does "
-                             "not match the given spectral ids")
-
-    def get_subset(self, ids):
-        """
-        Extract subset of the library with the passed spectrum ids. A
-        new TemplateLibrary will be returned.
-
-        For details, see SpectrumSet.get_subset.
-        """
-        new_set, index = self.spectrumset.get_subset(ids, get_selector=True)
-        new_catalog = self.catalog[index]
-        return TemplateLibrary(spectrumset=new_set, catalog=new_catalog)
-
-    def match_resolution(self, target_resolution):
-        """
-        Convolve the library spectra until their spectra resolution
-        matches that passed target_resolution. Does not return -
-        modifies the library spectra in place.
-
-        Args:
-        target_resolution - 1D arraylike
-            The desired spectral resolution, sampled with wavelength
-            identically to the library spectra. The units are assumed
-            to be Gaussian FWHM in the wavelength units of the library.
-        """
-        target_fwhm = np.asarray(target_resolution)
-        current_resolution = self.spectrumset.metaspectra["ir"]
-        delta_var = target_resolution**2 - current_resolution**2
-        res_matches = np.max(np.absolute(delta_var)) < const.float_tol
-        if res_matches:
-            warnings.warn("Templates already have the target spectral "
-                          "resolution, skipping convolution", RuntimeWarning)
-            return
-        decrease_fwhm = np.any(delta_var < 0.0)
-        if decrease_fwhm:
-            raise ValueError("Invalid target resolution - must be "
-                             "greater than the current resolution")
-        fwhm_to_add = np.sqrt(delta_var)
-        sigma_to_add = fwhm_to_add/const.gaussian_fwhm_over_sigma
-        self.spectrumset = self.spectrumset.gaussian_convolve(sigma_to_add)
-        return

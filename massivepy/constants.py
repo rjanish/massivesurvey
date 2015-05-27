d0e1dba726a41cc52545cca98a3150048d5a3da4
Ryan Janish <ryan.janish@gmail.com>
2015-05-27 11:38:45 -0700

    ngc re

--- massivepy/constants.py
+++ massivepy/constants.py
@@ -33,6 +33,7 @@ selected_twelve = [14, 227, 303, 311, 547, 561, 586, 591, 052, 755, 844, 871]
     # G and K, with a few M, stars spanning the available MILES range of
     # metallicity. Otherwise, they were chosen by-hand and mostly randomly.
 float_tol = 10**(-10)
+re_ngc = r"(?:NGC|ngc)[ _-]?(?P<num>\d{4})"
 
 # math
 gaussian_fwhm_over_sigma = 2*np.sqrt(2*np.log(2))

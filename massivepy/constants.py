b6c0f8c4f361a70b1964b9e5c5f98bce36bebd30
Ryan Janish <ryan.janish@gmail.com>
2015-05-29 13:01:59 -0700

    doc update

--- massivepy/constants.py
+++ massivepy/constants.py
@@ -1,5 +1,5 @@
 """
-Miscellaneous constants and unit definitions of general use.
+Miscellaneous constants and unit definitions.
 """
 
 
@@ -17,15 +17,15 @@ c_kms = astro_const.c.to('km/s').value
 
 # instrument constants
 mitchell_fiber_radius = 2.08 * arcsec
-mitchell_arc_centers = np.array([4046.5469,
-                                 4077.8403,
-                                 4358.3262,
-                                 4678.1558,
-                                 4799.9038,
-                                 4916.0962,
-                                 5085.8110,
-                                 5460.7397,
-                                 5769.5972])
+mitchell_arc_centers = np.array([4046.5469,  # Hg-Cd blend
+                                 4077.8403,  # Hg
+                                 4358.3262,  # Hg
+                                 4678.1558,  # Hg-Cd blend -dominated by Hg
+                                 4799.9038,  # Cd
+                                 4916.0962,  # Cd
+                                 5085.8110,  # Cd
+                                 5460.7397,  # Hg-Cd blend -dominated by Hg
+                                 5769.5972]) # Hg
 mitchell_nominal_spec_resolution = 4.6  # Angstroms, Gaussian FWHM
 mitchell_crop_region = [3650, 5650] # Angstrom, galaxy rest frame
     # edges can have goofy extrapolated data, this is a stable safe region
@@ -39,15 +39,15 @@ selected_twelve = [14, 227, 303, 311, 547, 561, 586, 591, 052, 755, 844, 871]
 fullMILES_1600fullgalaxy_optimized = [136, 246, 376, 498, 720, 838,
                                       956, 197, 252, 409, 548, 780,
                                       853, 225, 322, 455, 656, 835,  881]
-    # nonzero templates resulting from a ppxf of the full-galaxy bin of
-    # NGC1600 using the full miles-massive template library
     # DO NOT KEEP HERE - FOR TESTING PURPOSED ONLY
-
+    # Move this to an update-able store of similar list for all galaxies!
+    # These are the nonzero templates resulting from a ppxf of the full-
+    # galaxy bin of NGC1600 using the full miles-massive template library
 float_tol = 10**(-10)
-re_ngc = r"(?:NGC|ngc|N)(?P<num>\d{4})"
+re_ngc = r"(?:NGC|ngc|N)(?P<num>\d{4})" # get number from NGC designations
 
 # math
 gaussian_fwhm_over_sigma = 2*np.sqrt(2*np.log(2))
 
 # directory structure
-path_to_datamap = "etc/datamap.txt"
+path_to_datamap = "etc/datamap.txt" # file giving locations of data stores

5df1312655e8c0010811e7866240ebbcbb2f628f
melanieveale <melanie.veale@berkeley.edu>
2015-08-20 11:38:09 -0700

    cleaned up code and plots, got rid of old testing related stuff

--- massivepy/plot_s4.py
+++ massivepy/plot_s4.py
@@ -22,86 +22,51 @@ import massivepy.plot_massive as mplt
 from plotting.geo_utils import polar_box
 
 
-def plot_s4_postprocess(gal_name=None,plot_path=None,lambda_path=None,
-                        lambda_path_med=None,lambda_path_jstyle=None,
-                        fits_path=None,rprofiles_path=None):
+def plot_s4_postprocess(gal_name=None,plot_path=None,rprofiles_path=None):
     # get data
-    rprofiles = np.genfromtxt(rprofiles_path, names=True)
-    #lamR = np.genfromtxt(lambda_path,names=True)
-    #lamR_median = np.genfromtxt(lambda_path_med,names=True)
-    #lamR_jstyle = np.genfromtxt(lambda_path_jstyle,names=True)
-    labels = {'R': 'radius',
-              'Vavg': r'$\langle |V| \rangle$',
-              'RVavg': r'$\langle R |V| \rangle$',
-              'm2avg': r'$\langle \sqrt{V^2 + \sigma^2} \rangle$',
-              'Rm2avg': r'$\langle R \sqrt{V^2 + \sigma^2} \rangle$',
-              'lam': r'$\lambda_R$',
-              'V':r'$V$',
-              'Vraw':r'$V_{\rm raw}$',
+    rprofiles = np.genfromtxt(rprofiles_path, names=True, skip_header=1)
+    labels = {'lam': r'$\lambda_R$',
               'sigma':r'$\sigma$',
               'flux':'flux'}
+    ii = rprofiles['toplot'].astype(bool)
 
     ### Plotting Begins! ###
 
     pdf = PdfPages(plot_path)
 
     # plot the actual lambda first
-    fig = plt.figure(figsize=(6,5))
-    fig.suptitle(labels['lam'])
-    ax = fig.add_axes([0.17,0.15,0.7,0.7])
-    ax.plot(rprofiles['rbin'],rprofiles['lam'],c='b',label='flux avg')
-    ax.plot(rprofiles['rbin'],rprofiles['lam_med'],c='c',label='median')
-    ax.plot(rprofiles['rbin'],rprofiles['lam_old'],c='r',label='old median')
-    #ax.plot(lamR_jstyle['R'],lamR_jstyle['lam'],c='r',label='jstyle')
-    ax.set_xlabel('radius')
-    ax.set_ylabel(labels['lam'])
-    #if gal_name in ['NGC0057','NGC0507']:
-    #    Jf = os.path.join(os.path.dirname(plot_path),'jenny.txt')
-    #    Jthings = np.genfromtxt(Jf,names=True)
-    #    ax.plot(Jthings['rad'],Jthings['lam'],c='m',label='Jenny')
-    ax.legend()
-    pdf.savefig(fig)
-    plt.close(fig)
-
-    fig = plt.figure(figsize=(6,5))
-    fig.suptitle(labels['lam'])
-    ax = fig.add_axes([0.17,0.15,0.7,0.7])
-    ax.plot(rprofiles['rbin'],rprofiles['lam'],c='b',marker='s',label='all')
-    ii = rprofiles['rtoplot'].astype(bool)
-    ax.plot(rprofiles['rbin'][ii],rprofiles['lam'][ii],c='c',marker='o',label='good')
-    ax.set_xlabel('radius')
-    ax.set_ylabel(labels['lam'])
-    ax.legend()
-    pdf.savefig(fig)
-    plt.close(fig)
-
-    '''
-    # plot the intermediate steps as subplots on one page
     fig = plt.figure(figsize=(6,6))
-    fig.suptitle(r"Intermediate Steps (x-axis matching $\lambda_R$)")
-    for i, thing in enumerate(['Vavg','RVavg','m2avg','Rm2avg']):
-        ax = fig.add_subplot(2,2,i+1)
-        ax.plot(lamR['R'],lamR[thing],c='b')
-        ax.plot(lamR_median['R'],lamR_median[thing],c='c')
-        ax.plot(lamR_jstyle['R'],lamR_jstyle[thing],c='r')
-        if thing=='RVavg':
-            ax.plot(Jthings['rad'],Jthings['num'],c='m')
-            pass
-        elif thing=='Rm2avg':
-            ax.plot(Jthings['rad'],Jthings['denom'],c='m')
-            pass
-        ax.set_yscale('log')
-        ax.set_title(labels[thing])
-        ax.set_xticklabels([])
+    fig.suptitle(labels['lam'])
+    ax = fig.add_axes([0.15,0.1,0.8,0.7])
+    ax.plot(rprofiles['rencl'][ii],rprofiles['lam'][ii],
+            c='b',label='fiducial')
+    ax.plot(rprofiles['rencl'][ii],rprofiles['lam_med'][ii],
+            c='c',label='median for V0')
+    ax.plot(rprofiles['rencl'][ii],rprofiles['lam_fluxw'][ii],
+            c='r',label='flux weighted')
+    ax.set_xlabel('radius')
+    ax.set_ylabel(labels['lam'])
+    ax.legend(loc='lower center',bbox_to_anchor=(0.5,1),ncol=2)
     pdf.savefig(fig)
     plt.close(fig)
-    '''
 
-    fig, ax = mplt.scalarmap(figtitle='sigma stuff',xlabel='',ylabel='')
-    ax.plot(rprofiles['rbin'],rprofiles['sig'],c='b')
-    #if gal_name in ['NGC0057','NGC0507']:
-        # should already have Jthings
-    #    ax.plot(Jthings['rad'],Jthings['sig_weighted'],c='m')
+    fig = plt.figure(figsize=(6,6))
+    fig.suptitle(labels['lam'])
+    ax = fig.add_axes([0.15,0.1,0.8,0.7])
+    ax.plot(rprofiles['rencl'],rprofiles['lam'],
+            c='b',marker='s',ms=3,label='all points')
+    ax.plot(rprofiles['rencl'][ii],rprofiles['lam'][ii],
+            c='c',marker='o',ms=3,label='one per annulus')
+    ax.set_xlabel('radius')
+    ax.set_ylabel(labels['lam'])
+    ax.legend(loc='lower center',bbox_to_anchor=(0.5,1),ncol=2)
+    pdf.savefig(fig)
+    plt.close(fig)
+
+
+    fig, ax = mplt.scalarmap(figtitle=labels['sigma'],
+                             xlabel='radius',ylabel=labels['sigma'])
+    ax.plot(rprofiles['rencl'][ii],rprofiles['sig'][ii],c='b')
     pdf.savefig(fig)
     plt.close(fig)
 
--- s4_postprocess.py
+++ s4_postprocess.py
@@ -26,6 +26,7 @@ import utilities as utl
 import massivepy.constants as const
 import massivepy.io as mpio
 import massivepy.postprocess as post
+import massivepy.binning as binning
 from massivepy.plot_s4 import plot_s4_postprocess
 
 # get cmd line arguments
@@ -43,36 +44,29 @@ for paramfile_path in all_paramfile_paths:
     # parse input parameter file
     output_dir, gal_name = mpio.parse_paramfile_path(paramfile_path)
     input_params = utl.read_dict_file(paramfile_path)
-
-    bininfo_path = input_params['bininfo_path']
-    if not os.path.isfile(bininfo_path):
-        raise Exception("File {} does not exist".format(bininfo_path))
-    ppxf_output_path = input_params['ppxf_output_path']
-    if not os.path.isfile(ppxf_output_path):
-        raise Exception("File {} does not exist".format(ppxf_output_path))
     run_name = input_params['run_name']
+    bininfo_path = input_params['bininfo_path']
+    binfit_path = input_params['binfit_path']
+    check = mpio.pathcheck([bininfo_path,binfit_path],['.txt','.fits'],gal_name)
+    if not check:
+        print 'Something is wrong with the input paths for {}'.format(gal_name)
+        print 'Skipping to next galaxy.'
+        continue
     # construct output file names
     output_path_maker = lambda f,ext: os.path.join(output_dir,
                 "{}-s4-{}-{}.{}".format(gal_name,run_name,f,ext))
-    fits_path = output_path_maker('underconstruction','fits')
-    lambda_path = output_path_maker('lambda','txt')
+    # fits_path = output_path_maker('public','fits')
     plot_path = output_path_maker('lambda','pdf')
-    lambda_path_med = output_path_maker('lambda-med','txt')
-    lambda_path_jstyle = output_path_maker('lambda-jstyle','txt')
     rprofiles_path = output_path_maker('rprofiles','txt')
     # save relevant info for plotting to a dict
-    plot_info = {'fits_path': fits_path,
-                 'rprofiles_path': rprofiles_path,
-                 'lambda_path': lambda_path,
-                 'lambda_path_med': lambda_path_med,
-                 'lambda_path_jstyle': lambda_path_jstyle,
+    plot_info = {'rprofiles_path': rprofiles_path,
                  'plot_path': plot_path,
                  'gal_name': gal_name}
     things_to_plot.append(plot_info)
 
     # decide whether to continue with script or skip to plotting
     # only checks for "main" fits file, not whether params have changed
-    if os.path.isfile(fits_path):
+    if os.path.isfile(rprofiles_path):
         if input_params['skip_rerun']=='yes':
             print '\nSkipping re-run of {}, plotting only'.format(gal_name)
             continue
@@ -82,57 +76,57 @@ for paramfile_path in all_paramfile_paths:
             raise Exception("skip_rerun must be yes or no")
 
     # ingest required data
-    bininfo = np.genfromtxt(bininfo_path,names=True,skip_header=1)
-    #bininfo = np.genfromtxt(bininfo_path,names=True,skip_header=12)
-    fitdata = mpio.get_friendly_ppxf_output(ppxf_output_path)
-
-    # here is a placeholder for the code to write the public fits file
-    header = fits.Header()
-    hdu = fits.PrimaryHDU(data=bininfo['flux'],header=header)
-    fits.HDUList([hdu]).writeto(fits_path, clobber=True)
-
+    #bindata, binetc = binning.read_bininfo(bininfo_path)
+    bindata = np.genfromtxt(bininfo_path,names=True,skip_header=12)
+    fitdata = mpio.get_friendly_ppxf_output(binfit_path)
 
     # create a container for all of my radial profiles
-    n_rsteps = len(bininfo['r'])
-    dt = {'names': ['lastbin','rtoplot','rbin','rencl','lam','sig','lam_med','lam_old'],
+    n_rsteps = len(bindata['r'])
+    dt = {'names': ['lastbin','rbin','toplot','rencl','sig',
+                    'lam','lam_med','lam_fluxw'],
           'formats': ['i8'] + ['b'] + 6*['f8']}
     rprofiles = np.zeros(n_rsteps,dtype=dt)
-    ii = np.argsort(bininfo['r'])
-    rprofiles['rbin'] = bininfo['r'][ii]
-    outerbinr = bininfo['rmax'][ii]
-    jj = np.isnan(outerbinr)
-    outerbinr[jj] = bininfo['r'][ii][jj]
-    rdiffs = np.diff(outerbinr)
-    kk = np.nonzero(rdiffs)
-    rprofiles['rtoplot'] = False
-    rprofiles['rtoplot'][kk] = True
-    rprofiles['rtoplot'][-1] = True
+
+    # populate the radius information
+    ii = np.argsort(bindata['r'])
+    rprofiles['rbin'] = bindata['r'][ii]
+    rencl = bindata['rmax'][ii]
+    jj = np.isnan(rencl)
+    rencl[jj] = bindata['r'][ii][jj]
+    rprofiles['rencl'] = rencl
+    kk = np.nonzero(np.diff(rencl))
+    rprofiles['toplot'] = False
+    rprofiles['toplot'][kk] = True
+    rprofiles['toplot'][-1] = True
 
     # here is the calculation of lambda
-    lamR = post.calc_lambda(bininfo['r'],fitdata['gh']['moment'][:,0],
-                    fitdata['gh']['moment'][:,1],bininfo['flux'],Vnorm='median')
-    np.savetxt(lambda_path,lamR,header=' '.join(lamR.dtype.names))
-    rprofiles['lam_old'] = lamR['lam']
-    # do again using median
-    luminosities = bininfo['flux']*bininfo['nfibers']
-    lamR_median = post.calc_lambda(bininfo['r'],fitdata['gh']['moment'][:,0],
-                                  fitdata['gh']['moment'][:,1],luminosities,
-                                  Vnorm='median')
-    np.savetxt(lambda_path_med,lamR_median,header=' '.join(lamR.dtype.names))
-    rprofiles['lam_med'] = lamR_median['lam']
-    # do again with the correct weighting!
-    luminosities = bininfo['flux']*bininfo['nfibers']
-    lamR_new = post.calc_lambda(bininfo['r'],fitdata['gh']['moment'][:,0],
-                                fitdata['gh']['moment'][:,1],luminosities)
-    rprofiles['lam'] = lamR_new['lam']
+    r = bindata['r']
+    vel = fitdata['gh']['moment'][:,0]
+    sigma = fitdata['gh']['moment'][:,1]
+    flux = bindata['flux']
+    luminosity = flux*bindata['nfibers'] # skipping fiber_area since it cancels
+
+    lamR = post.calc_lambda(r,vel,sigma,luminosity)
+    rprofiles['lam'] = lamR['lam']
+    lamR = post.calc_lambda(r,vel,sigma,luminosity,Vnorm='median')
+    rprofiles['lam_med'] = lamR['lam']
+    lamR = post.calc_lambda(r,vel,sigma,flux)
+    rprofiles['lam_fluxw'] = lamR['lam']
     
     # do sigma thing
-    sig_fluxavg = post.calc_sigma(bininfo['r'],fitdata['gh']['moment'][:,1],
-                                  bininfo['flux'])
+    sig_fluxavg = post.calc_sigma(r,sigma,flux)
     rprofiles['sig'] = sig_fluxavg['sig']
 
     # save the radial profiles
-    np.savetxt(rprofiles_path,rprofiles,header=' '.join(rprofiles.dtype.names))
+    header = ('Columns are as follows:'
+              '\n {colnames}'
+              '\nMetadata is as follows:'
+              '\n (still need to set up metadata)'
+              '\nFor now, lam uses the flux-weighted average V as V0'
+              '\nlam is luminosity weighted except lam_fluxw'
+              '\nsig is flux-weighted average sigma within R'.format)
+    header = header(colnames=' '.join(rprofiles.dtype.names))
+    np.savetxt(rprofiles_path,rprofiles,header=header)
 
 for plot_info in things_to_plot:
     plot_s4_postprocess(**plot_info)

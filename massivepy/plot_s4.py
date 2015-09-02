6eae6f28c83eb1edf4ee705a8b94269fd0f1da36
melanieveale <melanie.veale@berkeley.edu>
2015-09-01 18:25:19 -0700

    updated s4 to take galaxy info from bininfo instead of reading from targets again

--- massivepy/plot_s4.py
+++ massivepy/plot_s4.py
@@ -26,7 +26,7 @@ from plotting.geo_utils import polar_box
 def plot_s4_postprocess(gal_name=None,plot_path=None,rprofiles_path=None):
     # get data
     rprofiles = np.genfromtxt(rprofiles_path, names=True, skip_header=1)
-    rmeta = post.read_rprofiles_header(rprofiles_path)
+    rmeta = mpio.read_friendly_header(rprofiles_path)
     labels = {'lam': r'$\lambda_R$',
               'sigma':r'$\sigma$',
               'flux':'flux'}
@@ -41,14 +41,14 @@ def plot_s4_postprocess(gal_name=None,plot_path=None,rprofiles_path=None):
     fig.suptitle(labels['lam'])
     ax = fig.add_axes([0.15,0.1,0.8,0.7])
     ax.plot(rprofiles['rencl'][ii],rprofiles['lam'][ii],c='k',label='fiducial')
-    ax.axhline(rmeta['sf_cutoff'],c='k',ls='--',label='slow/fast cutoff')
+    ax.axhline(rmeta['slow/fast cutoff'],c='k',ls='--',label='slow/fast cutoff')
     ax.plot(rprofiles['rencl'][ii],rprofiles['lam_minv0'][ii],c='b',
             label='min V0')
     ax.plot(rprofiles['rencl'][ii],rprofiles['lam_maxv0'][ii],c='g',
             label='max V0')
-    ax.axvline(rmeta['re'],c='k',ls=':',label=r'$R_e$')
-    ax.plot(rmeta['re'],rmeta['lambda_re'],ls='',marker='o',mfc='k')
-    ax.plot(0.5*rmeta['re'],rmeta['lambda_halfre'],ls='',marker='o',mfc='k')
+    ax.axvline(rmeta['gal re'],c='k',ls=':',label=r'$R_e$')
+    ax.plot(rmeta['gal re'],rmeta['lambda re'],ls='',marker='o',mfc='k')
+    ax.plot(.5*rmeta['gal re'],rmeta['lambda half re'],ls='',marker='o',mfc='k')
     ax.set_xlabel('radius')
     ax.set_ylabel(labels['lam'])
     ax.legend(loc='lower center',bbox_to_anchor=(0.5,1),ncol=3)
